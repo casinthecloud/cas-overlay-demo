c3e144fd275e50642b8985dd7cff8dd6e27ba1db
leleuj <leleuj@gmail.com>
2016-09-08 20:14:42 +0200

    name containers

--- docker.sh
+++ docker.sh
@@ -3,7 +3,7 @@
 mvn clean package
 cd cas-overlay-server-demo
 docker build -t cas-overlay-server-demo .
-docker run -p 8080:8080 -i -d cas-overlay-server-demo
+docker run --name server -p 127.0.0.1:8080:8080 -i -d cas-overlay-server-demo
 cd ../cas-overlay-management-demo/
 docker build -t cas-overlay-management-demo .
-docker run -p 8081:8081 -i -d cas-overlay-management-demo
+docker run --name management -p 127.0.0.1:8081:8081 -i -d cas-overlay-management-demo
---
+++ end.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+docker stop management server
+docker rm management server
